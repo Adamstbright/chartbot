a650e2ddcd14eca37a92a19753f40a978a79ae52
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-05-21 12:08:40 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QLineEdit, QVBoxLayout, QGridLayout, \
+    QPushButton
+from PyQt6.QtGui import QAction
+import sys
+import mysql.connector
+
+
